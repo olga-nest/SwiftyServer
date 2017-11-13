2e1cd612fb00641d24ba0cfb8d604c9539f4e906
olga-nest <olga.w.nesterova@gmail.com>
2017-11-13 12:26:12 -0800

    Add uppercase and uppercaseGet routes

--- Sources/App/Routes/Routes.swift
+++ Sources/App/Routes/Routes.swift
@@ -4,14 +4,30 @@ extension Droplet {
     func setupRoutes() throws {
         get("hello") { req in
             var json = JSON()
-            try json.set("hello", "world")
+            try json.set("hello", "world\n")
             return json
         }
 
         get("plaintext") { req in
-            return "Hello, world!"
+            return "Hello, world!\n"
+        }
+        
+        //Response to curl localhost:8080/uppercase/words
+        get("uppercase", ":words") { req in
+            guard let words = req.parameters["words"]?.string else {
+                return "Error retriving parameter/s\n"
+            }
+            return "\(words.uppercased())\n"
         }
 
+        //Response to curl -X "GET" "localhost:8080/uppercase?words=<your_word>"
+        get("uppercaseGet") { req in
+            guard let words = req.data["words"]?.string else {
+                return "Error retriving parameter/s\n"
+            }
+            return "\(words.uppercased())\n"
+        }
+        
         // response to requests to /info domain
         // with a description of the request
         get("info") { req in
