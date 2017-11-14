5742dc1de3bffb99c6c11d8aa12cc1feb9d2c56b
olga-nest <olga.w.nesterova@gmail.com>
2017-11-13 18:39:02 -0800

    Use user default to store data

--- Sources/App/Models/Store.swift
+++ Sources/App/Models/Store.swift
@@ -16,22 +16,47 @@ class Store: NSObject {
 //    var storeValue = ""
     var allInfo = [String: String]()
     
+    let defaults = UserDefaults.standard
+    
+    
     func set(key: String, value: String){
+       defaults.removeObject(forKey: "allInfo")
         allInfo[key] = value
+        if defaults.object(forKey: "allInfo") == nil {
+            setDict(dict: allInfo as NSDictionary)
+        } else {
+//        let object =
+            
+//            NSData *data = [NSKeyedArchiver archivedDataWithRootObject:jsonObject];
+//            [data writeToFile:path atomically:YES
+           defaults.setValue(value, forKey: key)
+        }
     }
     
     func allItems() -> [String] {
         var tempArr = [String]()
+        let dict = getDict()
         
-        for (k, v) in allInfo {
+        for (k, v) in dict  {
             tempArr.append("\(k) \(v)")
         }
-        
         return tempArr
     }
     
     func get(key: String) -> String? {
         return allInfo[key]
+    }
+    
+    func setDict(dict: NSDictionary) {
+        let data = NSKeyedArchiver.archivedData(withRootObject: dict)
+        defaults.set(data, forKey:"allInfo")
+    }
+    
+     func getDict() -> NSDictionary {
+        let data = defaults.object(forKey: "allInfo") as! NSData
+        let object = NSKeyedUnarchiver.unarchiveObject(with: data as Data) as! NSDictionary
+        let tempDict = object as! [String : String]
+        return tempDict as NSDictionary
     }
     
    
